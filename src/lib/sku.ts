17d0f83aa19a64a7fe7ab8509687dc39024db3f7
ศรัณญ์ ซือสุวรรณ <sarun@sranys-MacBook-Pro.local>
2024-08-23 15:46:38 +0700

    refactor/ get data for create order, merge APIs together

--- src/lib/sku.ts
+++ src/lib/sku.ts
@@ -1,18 +1,21 @@
-import {
-  addDoc,
-  collection,
-  doc,
-  setDoc,
-  getDoc,
-  where,
-  query,
-  getDocs,
-} from "firebase/firestore";
+import { collection, where, query, getDocs } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
+import { getOrderBySKU, getOrderLastDate } from "./order";
 
 interface SKUResType {
   ok: boolean;
   data?: SKUType;
+  error?: string;
+}
+
+interface CreateOrderDataResType {
+  ok: boolean;
+  data?: {
+    sku: SKUType;
+    order: OrderType;
+    lastOrder: OrderType;
+  };
+  error?: string;
 }
 
 //user collection
@@ -44,60 +47,99 @@ export const getSKUByBarcode = async (barcode: string) => {
   }
 };
 
-export const getSKU = (barcode: string) => {
-  return {
-    id: "1",
-    name: "น้ำดื่มสิงห์ 1500 มล",
-    apCode: "1111",
-    apName: "บุญรอดเทรดดิ้ง",
-    img: "",
-    catCode: "C0001",
-    catName: "น้ำดื่ม",
-    brnCode: "B0001",
-    brnName: "สิงห์",
-    goods: [
-      {
-        code: "8850999320007",
-        utqname: "ขวด",
-        utqqty: 1,
-        price0: 10,
-        price8: 10,
+export const getCreateOrderData = async (barcode: string, branch: string) => {
+  try {
+    const skuRes = await getSKUByBarcode(barcode);
+    if (!skuRes || !skuRes?.ok || !skuRes.data) {
+      return {
+        ok: false,
+        error: "Error in get create order data: \nSKU not found.",
+      } as CreateOrderDataResType;
+    }
+    const orderRes = await getOrderBySKU(skuRes.data.id, branch);
+    if (!orderRes?.ok) {
+      return {
+        ok: false,
+        error: "Error in get create order data: ",
+      } as CreateOrderDataResType;
+    }
+    const lastDateRes = await getOrderLastDate(skuRes.data.id, branch);
+    if (!lastDateRes?.ok) {
+      return {
+        ok: false,
+        error: "Error in get create order data: ",
+      } as CreateOrderDataResType;
+    }
+    return {
+      ok: true,
+      data: {
+        sku: skuRes.data,
+        order: orderRes.data,
+        lastOrder: lastDateRes.data,
       },
-      {
-        code: "8850999320021",
-        utqname: "แพค6",
-        utqqty: 6,
-        price0: 45,
-        price8: 42,
-      },
-      {
-        code: "1234000000021",
-        utqname: "แพค6",
-        utqqty: 6,
-        price0: 45,
-        price8: 42,
-      },
-      {
-        code: "1234000000022",
-        utqname: "แพค6",
-        utqqty: 6,
-        price0: 45,
-        price8: 42,
-      },
-      {
-        code: "1234000000023",
-        utqname: "แพค6",
-        utqqty: 6,
-        price0: 45,
-        price8: 42,
-      },
-      {
-        code: "1234000000024",
-        utqname: "แพค6",
-        utqqty: 6,
-        price0: 45,
-        price8: 42,
-      },
-    ],
-  } as SKUType;
+    } as CreateOrderDataResType;
+  } catch (err) {
+    return {
+      ok: false,
+      error: "Catch :" + err,
+    } as CreateOrderDataResType;
+  }
 };
+
+// export const getSKU = (barcode: string) => {
+//   return {
+//     id: "1",
+//     name: "น้ำดื่มสิงห์ 1500 มล",
+//     apCode: "1111",
+//     apName: "บุญรอดเทรดดิ้ง",
+//     img: "",
+//     catCode: "C0001",
+//     catName: "น้ำดื่ม",
+//     brnCode: "B0001",
+//     brnName: "สิงห์",
+//     goods: [
+//       {
+//         code: "8850999320007",
+//         utqname: "ขวด",
+//         utqqty: 1,
+//         price0: 10,
+//         price8: 10,
+//       },
+//       {
+//         code: "8850999320021",
+//         utqname: "แพค6",
+//         utqqty: 6,
+//         price0: 45,
+//         price8: 42,
+//       },
+//       {
+//         code: "1234000000021",
+//         utqname: "แพค6",
+//         utqqty: 6,
+//         price0: 45,
+//         price8: 42,
+//       },
+//       {
+//         code: "1234000000022",
+//         utqname: "แพค6",
+//         utqqty: 6,
+//         price0: 45,
+//         price8: 42,
+//       },
+//       {
+//         code: "1234000000023",
+//         utqname: "แพค6",
+//         utqqty: 6,
+//         price0: 45,
+//         price8: 42,
+//       },
+//       {
+//         code: "1234000000024",
+//         utqname: "แพค6",
+//         utqqty: 6,
+//         price0: 45,
+//         price8: 42,
+//       },
+//     ],
+//   } as SKUType;
+// };
